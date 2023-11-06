2a5ee0f5b9723a639b9f94eb492c324544f18e98
Illia Fedenko <illiafedenko69@gmail.com>
2023-11-06 11:41:12 +0200

    fix on navigation

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -28,7 +28,7 @@ const MainNavigator = ({parentNavigator}) => {
     <NavigationContainer>
       <>
         <Stack.Navigator
-          initialRouteName={'Message'}
+          initialRouteName={'Splash'}
           screenOptions={{headerShown: false}}>
           <>
             <Stack.Screen name="Splash" component={SplashScreen} />
