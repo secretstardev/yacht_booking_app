09d363b4dd80c719194debfd0235b5892ce9a006
Illia Fedenko <illiafedenko69@gmail.com>
2023-11-15 08:41:50 +0200

    add apple signin

--- src/screens/welcome.js
+++ src/screens/welcome.js
@@ -16,6 +16,8 @@ import IconButton from '../components/IconButton';
 import Toast from 'react-native-toast-message';
 // import FingerprintScanner from 'react-native-fingerprint-scanner';
 import auth from '@react-native-firebase/auth';
+import { appleAuth } from '@invertase/react-native-apple-authentication';
+import { v4 as uuid } from 'uuid'
 
 import {
   GoogleSignin,
@@ -82,6 +84,74 @@ const WelcomeScreen = ({navigation}) => {
       return false;
     }
   };
+
+  const onAppleButtonPress = async () => {
+    
+    try {
+      const rawNonce = uuid();
+      const state = uuid();
+
+      appleAuth.configure({
+        // The Service ID you registered with Apple
+        clientId: "com.example.client-android",
+
+        // Return URL added to your Apple dev console. We intercept this redirect, but it must still match
+        // the URL you provided to Apple. It can be an empty route on your backend as it's never called.
+        redirectUri: "https://example.com/auth/callback",
+
+        // [OPTIONAL]
+        // Scope.ALL (DEFAULT) = 'email name'
+        // Scope.Email = 'email';
+        // Scope.Name = 'name';
+        scope: appleAuthAndroid.Scope.ALL,
+
+        // [OPTIONAL]
+        // ResponseType.ALL (DEFAULT) = 'code id_token';
+        // ResponseType.CODE = 'code';
+        // ResponseType.ID_TOKEN = 'id_token';
+        responseType: appleAuthAndroid.ResponseType.ALL,
+
+        // [OPTIONAL]
+        // A String value used to associate a client session with an ID token and mitigate replay attacks.
+        // This value will be SHA256 hashed by the library before being sent to Apple.
+        // This is required if you intend to use Firebase to sign in with this credential.
+        // Supply the response.id_token and rawNonce to Firebase OAuthProvider
+        nonce: rawNonce,
+
+        // [OPTIONAL]
+        // Unique state value used to prevent CSRF attacks. A UUID will be generated if nothing is provided.
+        state,
+      });
+
+      const response = await appleAuth.signIn();
+      if (response) {
+        const code = response.code; // Present if selected ResponseType.ALL / ResponseType.CODE
+        const id_token = response.id_token; // Present if selected ResponseType.ALL / ResponseType.ID_TOKEN
+        const user = response.user; // Present when user first logs in using appleId
+        const state = response.state; // A copy of the state value that was passed to the initial request.
+        console.log("Got auth code", code);
+        console.log("Got id_token", id_token);
+        console.log("Got user", user);
+        console.log("Got state", state);
+      }
+    } catch (error) {
+      if (error && error.message) {
+        switch (error.message) {
+          case appleAuthAndroid.Error.NOT_CONFIGURED:
+            console.log("appleAuthAndroid not configured yet.");
+            break;
+          case appleAuthAndroid.Error.SIGNIN_FAILED:
+            console.log("Apple signin failed.");
+            break;
+          case appleAuthAndroid.Error.SIGNIN_CANCELLED:
+            console.log("User cancelled Apple signin.");
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
 
   const revokeAccess = async () => {
     try {
@@ -156,7 +226,9 @@ const WelcomeScreen = ({navigation}) => {
               }}
             />
             <View style={styles.buttonContainer}>
-              <IconButton icon="apple" onPress={revokeAccess} />
+              <IconButton icon="apple" onPress={
+                async () => onAppleButtonPress()
+              } />
               <IconButton
                 icon="google"
                 onPress={async () => {
